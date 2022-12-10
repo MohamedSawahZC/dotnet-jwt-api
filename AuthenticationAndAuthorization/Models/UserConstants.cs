8cd17fea2fc88298e9371a5010cc094af9d44acb
Mohamed Sawah <61028956+MohamedSawahZC@users.noreply.github.com>
2022-12-10 23:20:54 +0200

    Finish user model and login model

---
+++ AuthenticationAndAuthorization/Models/UserConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthenticationAndAuthorization.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel() { UserName = "jason_admin", EmailAddress = "jason.admin@email.com", Password = "MyPass_w0rd", GivenName = "Jason", Surname = "Bryant", Role = "Administrator" },
+            new UserModel() { UserName = "elyse_seller", EmailAddress = "elyse.seller@email.com", Password = "MyPass_w0rd", GivenName = "Elyse", Surname = "Lambert", Role = "Seller" },
+        };
+    }
+}
